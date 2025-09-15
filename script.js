1b90ef04121484321b5b061888ee7028cd87598e
g4ieku <121449657+g4ieku@users.noreply.github.com>
2025-09-15 08:51:51 +0200

    add Clear UI State

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const itemFilter = document.querySelector("#filter");
+const items = itemList.querySelectorAll("li");
 const clearBtn = document.querySelector("#clear");
 
 const addItem = (event) => {
   event.preventDefault();
 
   const newItem = itemInput.value;
-  console.log(newItem);
 
   if (newItem === "") {
     alert("Please add an item!");
   } else {
     createNewItem(newItem);
+
+    checkUI();
+
     itemInput.value = null;
   }
 };
 
 const createNewItem = (item) => {
-  console.log(item);
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(item));
 
@@ -36,16 +39,47 @@ const createNewItem = (item) => {
 
 const removeItem = (event) => {
   if (event.target.parentElement.classList.contains("remove-item")) {
-    event.target.parentElement.parentElement.remove();
+    const itemName =
+      event.target.parentElement.parentElement.textContent.trim();
+    if (confirmation(`Are you sure you want to delete ${itemName}?`)) {
+      event.target.parentElement.parentElement.remove();
+    }
   }
+
+  checkUI();
 };
 
 const removeAllItems = () => {
-  itemList.querySelectorAll("li").forEach((item) => {
-    item.remove();
-  });
+  if (confirmation("Are you sure you want to delete all items?")) {
+    items.forEach((item) => {
+      item.remove();
+    });
+  }
+
+  checkUI();
+};
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+};
+
+const confirmation = (text) => {
+  if (confirm(text)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", removeAllItems);
+checkUI();
